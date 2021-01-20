0cb9556ae15e2ff87efa1b5de9141d92bb924b9e
Shotaro Kamimura <shotaro.kamimura@gmail.com>
2021-01-20 11:43:52 +0900

    file upload
    file update

---
+++ docs/garally.js
@@ -0,0 +1,17 @@
+const btn = () => {
+  document.getElementById("first").style.display ='block'
+  document.getElementById("second").style.display ='none'
+  document.getElementById("third").style.display ='none'
+}
+
+const btn2 = () => {
+  document.getElementById("second").style.display ='block'
+  document.getElementById("first").style.display ='none'
+  document.getElementById("third").style.display ='none'
+}
+
+const btn3 = () => {
+  document.getElementById("third").style.display ='block'
+  document.getElementById("first").style.display ='none'
+  document.getElementById("second").style.display ='none'
+}
